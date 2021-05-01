4355fa21cfc17797f6be88ad40eb8fd1042f365a
Rocky_ <1315930212@qq.com>
2021-05-01 19:12:22 +0800

    优化调试模式; 增加梦幻音乐随变模式; 使用声强传感器对应音频变化; 重命名项目为DBMAL; 版本号更新; Release版本准备

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,19 +1,20 @@
-# 1 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino"
+# 1 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
         
-# 1 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino"
-#pragma message "DBML(Arduino-Demo) version 1.001.000"
-# 1 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino"
+# 1 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
+#pragma message "DBMAL(Arduino-Demo) version 1.007.001"
+# 1 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
 
 
-# 4 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino" 2
+# 4 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino" 2
 
 
 
 
 
-# 10 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino" 2
-# 26 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino"
+# 10 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino" 2
+# 31 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
 // 返回限流后的值
+
 
 
 
@@ -36,9 +37,9 @@ int16_t db = 0; // 声强传感器虚拟值
 int8_t mirage_choice = 0; // 幻彩方案选择变量
 int8_t music_choice = 0; // 音乐方案选择变量
 
-CRGB leds[3 /* 灯条数量*/][40 /* 每排灯条所含灯珠数量*/]; // 三排灯珠RGB颜色数组
-CRGB leds_target[3 /* 灯条数量*/]
-                [40 /* 每排灯条所含灯珠数量*/]; // 三排灯珠RGB变换目标颜色数组
+CRGB leds[2 /* 灯条数量*/][62 /* 每排灯条所含灯珠数量*/]; // 三排灯珠RGB颜色数组
+CRGB leds_target[2 /* 灯条数量*/]
+                [62 /* 每排灯条所含灯珠数量*/]; // 三排灯珠RGB变换目标颜色数组
 
 // 系统主定时中断函数
 void DBML_Event();
@@ -54,22 +55,29 @@ void Strip_Update(); // 灯条颜色更新(立即)
 void Strip_Close(); // 关灯
 void Strip_Solid(); // 常亮照明模式
 void Strip_Mirage(int8_t mm_choice = 0); // 幻彩氛围模式
-void Strip_Music(int8_t mm_choice = 0, int16_t db_step = 103); // 音乐模式
+void Strip_Music(int8_t mm_choice = 0, int16_t db_step = 1024); // 音乐模式
 
 // 幻彩&音乐相关函数
 void MM_Dusk(); // 夕阳西下模式
 void MM_Disco(int16_t db_step = 103); // 蹦迪模式
+void MM_Fantasy(int16_t db = 1024); // 梦幻模式
 
 void setup() {
     step = 1; // 颜色渐变步长初始化
     db = analogRead(9 /* 声强传感器对应虚拟端*/); // 声强传感器虚拟值初始化
     mirage_choice = 0; // 幻彩方案选择变量初始化
-    music_choice = 0; ` // 音乐方案选择变量初始化
+    music_choice = 1; // 音乐方案选择变量初始化
 
     // 台灯状态初始化
     state_controller.switch_counter = 0;
     state_controller.switch_state = RELEASE;
-    state_controller.state = CLOSE;
+
+    randomSeed(analogRead(0));
+
+
+    state_controller.state = MUSIC;
+
+
 
     Switch_Init();
     Strip_Init();
@@ -79,8 +87,6 @@ void setup() {
 
 
     SerialUSB.begin(115200);
-    while (SerialUSB)
-        ;
 
 }
 
@@ -93,6 +99,24 @@ void loop() {
 
     SerialUSB.print("Voice: ");
     SerialUSB.println(db);
+    SerialUSB.print("Switch_Pressed: ");
+    SerialUSB.println(digitalRead(10 /* 感应开关对应数字端*/));
+    SerialUSB.print("Switch_State: ");
+    SerialUSB.println(state_controller.state);
+    SerialUSB.print("Switch_Counter: ");
+    SerialUSB.println(state_controller.switch_counter);
+    SerialUSB.print("Strip_Real_RGB: ");
+    SerialUSB.print(leds[0][0].r);
+    SerialUSB.print(" ");
+    SerialUSB.print(leds[0][0].g);
+    SerialUSB.print(" ");
+    SerialUSB.println(leds[0][0].b);
+    SerialUSB.print("Strip_Target_RGB: ");
+    SerialUSB.print(leds_target[0][0].r);
+    SerialUSB.print(" ");
+    SerialUSB.print(leds_target[0][0].g);
+    SerialUSB.print(" ");
+    SerialUSB.println(leds_target[0][0].b);
 
 }
 
@@ -101,7 +125,7 @@ void loop() {
  *                                系统相关函数 *
 
  *******************************************************************************/
-# 112 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino"
+# 141 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
 // 系统主定时函数
 void DBML_Event() {
     Switch_State_Update();
@@ -120,7 +144,7 @@ void DBML_Event() {
             break;
 
         case MUSIC:
-            Strip_Music(music_choice);
+            Strip_Music(music_choice, db);
             break;
     }
     Strip_Update_Fade(step); // 台灯颜色渐变式刷新
@@ -134,15 +158,15 @@ void DBML_Event() {
  *                                感应开关相关函数 *
 
  *******************************************************************************/
-# 142 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino"
+# 171 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
 // 感应开关初始化函数
 void Switch_Init() { pinMode(10 /* 感应开关对应数字端*/, (0x0)); }
 
 // 检测按键状态并更新台灯状态函数
 void Switch_State_Update() {
-    if (digitalRead(10 /* 感应开关对应数字端*/) == 1) {
+    if (digitalRead(10 /* 感应开关对应数字端*/)) {
         state_controller.switch_state = TOUCH;
-        state_controller.switch_counter++;
+        state_controller.switch_counter += 1;
     } else {
         state_controller.switch_state = RELEASE;
 
@@ -153,8 +177,9 @@ void Switch_State_Update() {
                 state_controller.state = SOLID;
             } else if (state_controller.state == SOLID) {
                 state_controller.state = MIRAGE;
-            } else if (state_controller.state == MIRAGE ||
-                       state_controller.state == MUSIC) {
+            } else if (state_controller.state == MIRAGE) {
+                state_controller.state = MUSIC;
+            } else if (state_controller.state == MUSIC) {
                 state_controller.state = SOLID;
             }
         }
@@ -172,33 +197,35 @@ void Switch_State_Update() {
  *                                 灯条相关函数 *
 
  *******************************************************************************/
-# 177 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino"
+# 207 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
 // 灯条初始化函数
 void Strip_Init() {
-    FastLED.addLeds<WS2812, 6 /* 左排灯条对应数字端*/, GRB>(leds[0], 40 /* 每排灯条所含灯珠数量*/);
-    FastLED.addLeds<WS2812, 7 /* 中排灯条对应数字端*/, GRB>(leds[1], 40 /* 每排灯条所含灯珠数量*/);
-    FastLED.addLeds<WS2812, 8 /* 右排灯条对应数字端*/, GRB>(leds[2], 40 /* 每排灯条所含灯珠数量*/);
-    for (int16_t i = 0; i < 3 /* 灯条数量*/; i++) {
-        for (int16_t j = 0; j < 40 /* 每排灯条所含灯珠数量*/; j++) {
+    FastLED.addLeds<WS2812, 1 /* 左排灯条对应数字端*/, GRB>(leds[0], 62 /* 每排灯条所含灯珠数量*/);
+    FastLED.addLeds<WS2812, 2 /* 中排灯条对应数字端*/, GRB>(leds[1], 62 /* 每排灯条所含灯珠数量*/);
+    // FastLED.addLeds<WS2812, LED_PIN_RIGHT, GRB>(leds[2], NUM_LEDS_PER_STRIP);
+    for (int16_t i = 0; i < 2 /* 灯条数量*/; i++) {
+        for (int16_t j = 0; j < 62 /* 每排灯条所含灯珠数量*/; j++) {
             leds[i][j] = CRGB::Black; // 保证初始化时所有灯珠全灭
         }
     }
-    FastLED.show();
 }
 
 // 灯条颜色更新(渐变)
 void Strip_Update_Fade(int8_t step) {
-    step = (((step) > (0) ? (step) : (0)) < (10) ? ((step) > (0) ? (step) : (0)) : (10)); // 防止step速度为负
+    step = (((0)) > (((step) < (51)) ? (step) : (51))) ? ((0)) : (((step) < (51)) ? (step) : (51)); // 防止step速度为负
 
     // 灯珠实际颜色渐变为目标颜色
-    for (int16_t i = 0; i < 3 /* 灯条数量*/; i++) {
-        for (int16_t j = 0; j < 40 /* 每排灯条所含灯珠数量*/; j++) {
-            leds[i][j].r +=
-                (((leds_target[i][j].r - leds[i][j].r) > ((-1 * step)) ? (leds_target[i][j].r - leds[i][j].r) : ((-1 * step))) < (step) ? ((leds_target[i][j].r - leds[i][j].r) > ((-1 * step)) ? (leds_target[i][j].r - leds[i][j].r) : ((-1 * step))) : (step));
-            leds[i][j].g +=
-                (((leds_target[i][j].g - leds[i][j].r) > ((-1 * step)) ? (leds_target[i][j].g - leds[i][j].r) : ((-1 * step))) < (step) ? ((leds_target[i][j].g - leds[i][j].r) > ((-1 * step)) ? (leds_target[i][j].g - leds[i][j].r) : ((-1 * step))) : (step));
-            leds[i][j].b +=
-                (((leds_target[i][j].b - leds[i][j].r) > ((-1 * step)) ? (leds_target[i][j].b - leds[i][j].r) : ((-1 * step))) < (step) ? ((leds_target[i][j].b - leds[i][j].r) > ((-1 * step)) ? (leds_target[i][j].b - leds[i][j].r) : ((-1 * step))) : (step));
+    for (int16_t i = 0; i < 2 /* 灯条数量*/; i++) {
+        for (int16_t j = 0; j < 62 /* 每排灯条所含灯珠数量*/; j++) {
+            leds[i][j].r += ((((-1 * step))) > ((((int16_t)((int16_t)leds_target[i][j].r - (int16_t)leds[i][j].r)) < (step)) ? ((int16_t)((int16_t)leds_target[i][j].r - (int16_t)leds[i][j].r)) : (step))) ? (((-1 * step))) : ((((int16_t)((int16_t)leds_target[i][j].r - (int16_t)leds[i][j].r)) < (step)) ? ((int16_t)((int16_t)leds_target[i][j].r - (int16_t)leds[i][j].r)) : (step))
+
+                                  ;
+            leds[i][j].g += ((((-1 * step))) > ((((int16_t)((int16_t)leds_target[i][j].g - (int16_t)leds[i][j].g)) < (step)) ? ((int16_t)((int16_t)leds_target[i][j].g - (int16_t)leds[i][j].g)) : (step))) ? (((-1 * step))) : ((((int16_t)((int16_t)leds_target[i][j].g - (int16_t)leds[i][j].g)) < (step)) ? ((int16_t)((int16_t)leds_target[i][j].g - (int16_t)leds[i][j].g)) : (step))
+
+                                  ;
+            leds[i][j].b += ((((-1 * step))) > ((((int16_t)((int16_t)leds_target[i][j].b - (int16_t)leds[i][j].b)) < (step)) ? ((int16_t)((int16_t)leds_target[i][j].b - (int16_t)leds[i][j].b)) : (step))) ? (((-1 * step))) : ((((int16_t)((int16_t)leds_target[i][j].b - (int16_t)leds[i][j].b)) < (step)) ? ((int16_t)((int16_t)leds_target[i][j].b - (int16_t)leds[i][j].b)) : (step))
+
+                                  ;
         }
     }
 }
@@ -206,8 +233,8 @@ void Strip_Update_Fade(int8_t step) {
 // 灯条颜色更新(立即)
 void Strip_Update() {
     // 灯珠实际颜色直接变换为目标颜色, 不经历渐变
-    for (int16_t i = 0; i < 3 /* 灯条数量*/; i++) {
-        for (int16_t j = 0; j < 40 /* 每排灯条所含灯珠数量*/; j++) {
+    for (int16_t i = 0; i < 2 /* 灯条数量*/; i++) {
+        for (int16_t j = 0; j < 62 /* 每排灯条所含灯珠数量*/; j++) {
             leds[i][j] = leds_target[i][j];
         }
     }
@@ -217,8 +244,8 @@ void Strip_Update() {
 void Strip_Close() {
     step = 1;
 
-    for (int16_t i = 0; i < 3 /* 灯条数量*/; i++) {
-        for (int16_t j = 0; j < 40 /* 每排灯条所含灯珠数量*/; j++) {
+    for (int16_t i = 0; i < 2 /* 灯条数量*/; i++) {
+        for (int16_t j = 0; j < 62 /* 每排灯条所含灯珠数量*/; j++) {
             leds_target[i][j] = CRGB::Black; // 保证初始化时所有灯珠全灭
         }
     }
@@ -228,8 +255,8 @@ void Strip_Close() {
 void Strip_Solid() {
     step = 1;
 
-    for (int16_t i = 0; i < 3 /* 灯条数量*/; i++) {
-        for (int16_t j = 0; j < 40 /* 每排灯条所含灯珠数量*/; j++) {
+    for (int16_t i = 0; i < 2 /* 灯条数量*/; i++) {
+        for (int16_t j = 0; j < 62 /* 每排灯条所含灯珠数量*/; j++) {
             leds_target[i][j] = (CRGB(255, 248, 220)) /* 纯色照明模式下灯珠颜色*/; // 灯珠颜色变为暖白
         }
     }
@@ -245,6 +272,10 @@ void Strip_Mirage(int8_t mm_choice) {
         case 1:
             MM_Disco();
             break;
+
+        case 2:
+            MM_Fantasy();
+            break;
     }
 }
 
@@ -254,6 +285,10 @@ void Strip_Music(int8_t mm_choice, int16_t db_step) {
         case 0:
             MM_Disco(db_step);
             break;
+
+        case 1:
+            MM_Fantasy(db_step);
+            break;
     }
 }
 
@@ -262,7 +297,7 @@ void Strip_Music(int8_t mm_choice, int16_t db_step) {
  *                               幻彩&音乐相关函数 *
 
  *******************************************************************************/
-# 264 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBML.ino"
+# 304 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
 // 夕阳西下模式
 void MM_Dusk() {
     // 夕阳西下模式下, HSV模式下颜色变换
@@ -272,12 +307,12 @@ void MM_Dusk() {
     // 颜色变化较缓慢
     step = 1;
 
-    for (int16_t i = 0; i < 3 /* 灯条数量*/; i++) {
-        for (int16_t j = 0; j < 40 /* 每排灯条所含灯珠数量*/; j++) {
+    for (int16_t i = 0; i < 2 /* 灯条数量*/; i++) {
+        for (int16_t j = 0; j < 62 /* 每排灯条所含灯珠数量*/; j++) {
             if (leds[i][j] == leds_target[i][j]) {
                 leds_target[i][j] =
                     CHSV(random(H_Min, H_Max + 1), random(240, 255 + 1),
-                         random(230, 255 + 1));
+                         random(200, 255 + 1));
             }
         }
     }
@@ -285,14 +320,48 @@ void MM_Dusk() {
 
 // 蹦迪模式
 void MM_Disco(int16_t db_step) {
-    //根据输入分贝的大小决定步长
-    step = ((float)db_step / 1024.0f) * 10.0f;
+    step = 10;
 
     // 颜色变化较快
-    for (int16_t i = 0; i < 3 /* 灯条数量*/; i++) {
-        for (int16_t j = 0; j < 40 /* 每排灯条所含灯珠数量*/; j++) {
+    for (int16_t i = 0; i < 2 /* 灯条数量*/; i++) {
+        for (int16_t j = 0; j < 62 /* 每排灯条所含灯珠数量*/; j++) {
             if (leds[i][j] == leds_target[i][j]) {
-                leds_target[i][j] = CRGB(random(255 + 1), 0, random(255 + 1));
+                leds_target[i][j] =
+                    CRGB(random(255 + 1), random(255 + 1), random(255 + 1));
+            }
+        }
+    }
+}
+
+// 梦幻模式
+void MM_Fantasy(int16_t db) {
+    float color_rate =
+        ((10.0f) > (((float)db - (float)250))) ? (10.0f) : (((float)db - (float)250)) / (float)((315) - (250));
+
+    step = color_rate * 6.0f;
+
+    //根据输入分贝的大小决定颜色群数量
+    int16_t group_num = color_rate * (float)8 /* 颜色集群最大数量*/;
+
+    group_num = ((1) > (group_num)) ? (1) : (group_num);
+
+    int16_t group_width = (float)62 /* 每排灯条所含灯珠数量*/ / (float)group_num;
+
+    int16_t color_down_bias = 255.0f - (color_rate * 255.0f);
+
+    CRGB color_group[4] = {
+        {random(color_down_bias, 255 + 1), random(color_down_bias, 255 + 1), 0},
+        {random(color_down_bias, 255 + 1), random(color_down_bias, 255 + 1),
+         random(color_down_bias, 255 + 1)},
+        {random(color_down_bias, 255 + 1), 0, random(color_down_bias, 255 + 1)},
+        {0, random(color_down_bias, 255 + 1),
+         random(color_down_bias, 255 + 1)}};
+
+    // 颜色变化较快
+    for (int16_t i = 0; i < 2 /* 灯条数量*/; i++) {
+        for (int16_t j = 0; j < 62 /* 每排灯条所含灯珠数量*/; j++) {
+            if (leds[i][j] == leds_target[i][j]) {
+                leds_target[i][j] = color_group[j / group_width % 4];
             }
         }
     }
--- build/sketch/DBMAL.ino.cpp
+++ build/sketch/DBMAL.ino.cpp
@@ -1,4 +1,6 @@
-#pragma message "DBML(Arduino-Demo) version 1.001.000"
+#include <Arduino.h>
+#line 1 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
+#pragma message "DBMAL(Arduino-Demo) version 1.007.001"
 
 #include <FastLED.h>
 
@@ -9,14 +11,19 @@
 #include <SAMDTimerInterrupt.h>
 #endif
 
-#define NUM_STRIPS 3           // 灯条数量
-#define NUM_LEDS_PER_STRIP 40  // 每排灯条所含灯珠数量
+#define NUM_STRIPS 2           // 灯条数量
+#define NUM_LEDS_PER_STRIP 62  // 每排灯条所含灯珠数量
 
-#define LED_PIN_LEFT 6   // 左排灯条对应数字端
-#define LED_PIN_MID 7    // 中排灯条对应数字端
+#define LED_PIN_LEFT 1   // 左排灯条对应数字端
+#define LED_PIN_MID 2    // 中排灯条对应数字端
 #define LED_PIN_RIGHT 8  // 右排灯条对应数字端
 #define VOLUME_PIN 9     // 声强传感器对应虚拟端
 #define SWITCH_PIN 10    // 感应开关对应数字端
+
+#define TOP_COLOR_GROUP_NUM 8  // 颜色集群最大数量
+#define VOICE_UP_DB 315
+#define VOICE_DOWN_DB 250
+#define VOICE_WIDTH_DB ((VOICE_UP_DB) - (VOICE_DOWN_DB))
 
 #define SYSTEM_INTERVAL_MS 8            // 系统频率在125hz左右
 #define LONG_HOLDING_COUNTER_LIMIT 250  // 按住超过2秒视为长按
@@ -24,9 +31,10 @@
 #define WARM_WHITE (CRGB(255, 248, 220))  // 纯色照明模式下灯珠颜色
 
 // 返回限流后的值
-#define LIMIT(val, min, max)                                                   \
-    (((val) > (min) ? (val) : (min)) < (max) ? ((val) > (min) ? (val) : (min)) \
-                                             : (max))
+#define MAX(a, b) ((a) > (b)) ? (a) : (b)
+#define MIN(a, b) ((a) < (b)) ? (a) : (b)
+
+#define LIMIT(val, min, max) MAX((min), MIN(val, max))
 
 enum SWITCH_STATE { RELEASE = 0, TOUCH };        // 开关状态枚举量
 enum STATE { CLOSE = 0, SOLID, MIRAGE, MUSIC };  // 全局状态枚举量
@@ -64,23 +72,43 @@ void Strip_Update();                      // 灯条颜色更新(立即)
 void Strip_Close();                       // 关灯
 void Strip_Solid();                       // 常亮照明模式
 void Strip_Mirage(int8_t mm_choice = 0);  // 幻彩氛围模式
-void Strip_Music(int8_t mm_choice = 0, int16_t db_step = 103);  // 音乐模式
+void Strip_Music(int8_t mm_choice = 0, int16_t db_step = 1024);  // 音乐模式
 
 // 幻彩&音乐相关函数
 void MM_Dusk();                        // 夕阳西下模式
 void MM_Disco(int16_t db_step = 103);  // 蹦迪模式
+void MM_Fantasy(int16_t db = 1024);    // 梦幻模式
 
+#line 80 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
+void setup();
+#line 108 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
+void loop();
+#line 272 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
+void Strip_Mirage(int8_t mm_choice);
+#line 289 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
+void Strip_Music(int8_t mm_choice, int16_t db_step);
+#line 325 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
+void MM_Disco(int16_t db_step);
+#line 340 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
+void MM_Fantasy(int16_t db);
+#line 80 "c:\\Personal\\University\\Code\\C_CPP\\Arduino_IDE\\DBML\\DBMAL.ino"
 void setup() {
     step = 1;                     // 颜色渐变步长初始化
     db = analogRead(VOLUME_PIN);  // 声强传感器虚拟值初始化
     mirage_choice = 0;            // 幻彩方案选择变量初始化
-    music_choice = 0;             // 音乐方案选择变量初始化
+    music_choice = 1;             // 音乐方案选择变量初始化
 
     // 台灯状态初始化
     state_controller.switch_counter = 0;
     state_controller.switch_state = RELEASE;
-    state_controller.state = CLOSE;
 
+    randomSeed(analogRead(0));
+
+#if DEBUG_MODE
+    state_controller.state = MUSIC;
+#else
+    state_controller.state = CLOSE;
+#endif
     Switch_Init();
     Strip_Init();
 
@@ -89,8 +117,6 @@ void setup() {
 #endif
 #if DEBUG_MODE
     Serial.begin(115200);
-    while (Serial)
-        ;
 #endif
 }
 
@@ -103,6 +129,24 @@ void loop() {
 #if DEBUG_MODE
     Serial.print("Voice: ");
     Serial.println(db);
+    Serial.print("Switch_Pressed: ");
+    Serial.println(digitalRead(SWITCH_PIN));
+    Serial.print("Switch_State: ");
+    Serial.println(state_controller.state);
+    Serial.print("Switch_Counter: ");
+    Serial.println(state_controller.switch_counter);
+    Serial.print("Strip_Real_RGB: ");
+    Serial.print(leds[0][0].r);
+    Serial.print(" ");
+    Serial.print(leds[0][0].g);
+    Serial.print(" ");
+    Serial.println(leds[0][0].b);
+    Serial.print("Strip_Target_RGB: ");
+    Serial.print(leds_target[0][0].r);
+    Serial.print(" ");
+    Serial.print(leds_target[0][0].g);
+    Serial.print(" ");
+    Serial.println(leds_target[0][0].b);
 #endif
 }
 
@@ -127,7 +171,7 @@ void DBML_Event() {
             break;
 
         case MUSIC:
-            Strip_Music(music_choice);
+            Strip_Music(music_choice, db);
             break;
     }
     Strip_Update_Fade(step);  // 台灯颜色渐变式刷新
@@ -144,9 +188,9 @@ void Switch_Init() { pinMode(SWITCH_PIN, INPUT); }
 
 // 检测按键状态并更新台灯状态函数
 void Switch_State_Update() {
-    if (digitalRead(SWITCH_PIN) == 1) {
+    if (digitalRead(SWITCH_PIN)) {
         state_controller.switch_state = TOUCH;
-        state_controller.switch_counter++;
+        state_controller.switch_counter += 1;
     } else {
         state_controller.switch_state = RELEASE;
 
@@ -157,8 +201,9 @@ void Switch_State_Update() {
                 state_controller.state = SOLID;
             } else if (state_controller.state == SOLID) {
                 state_controller.state = MIRAGE;
-            } else if (state_controller.state == MIRAGE ||
-                       state_controller.state == MUSIC) {
+            } else if (state_controller.state == MIRAGE) {
+                state_controller.state = MUSIC;
+            } else if (state_controller.state == MUSIC) {
                 state_controller.state = SOLID;
             }
         }
@@ -178,28 +223,30 @@ void Switch_State_Update() {
 void Strip_Init() {
     FastLED.addLeds<WS2812, LED_PIN_LEFT, GRB>(leds[0], NUM_LEDS_PER_STRIP);
     FastLED.addLeds<WS2812, LED_PIN_MID, GRB>(leds[1], NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<WS2812, LED_PIN_RIGHT, GRB>(leds[2], NUM_LEDS_PER_STRIP);
+    // FastLED.addLeds<WS2812, LED_PIN_RIGHT, GRB>(leds[2], NUM_LEDS_PER_STRIP);
     for (int16_t i = 0; i < NUM_STRIPS; i++) {
         for (int16_t j = 0; j < NUM_LEDS_PER_STRIP; j++) {
             leds[i][j] = CRGB::Black;  // 保证初始化时所有灯珠全灭
         }
     }
-    FastLED.show();
 }
 
 // 灯条颜色更新(渐变)
 void Strip_Update_Fade(int8_t step) {
-    step = LIMIT(step, 0, 10);  // 防止step速度为负
+    step = LIMIT(step, 0, 51);  // 防止step速度为负
 
     // 灯珠实际颜色渐变为目标颜色
     for (int16_t i = 0; i < NUM_STRIPS; i++) {
         for (int16_t j = 0; j < NUM_LEDS_PER_STRIP; j++) {
-            leds[i][j].r +=
-                LIMIT(leds_target[i][j].r - leds[i][j].r, (-1 * step), step);
-            leds[i][j].g +=
-                LIMIT(leds_target[i][j].g - leds[i][j].r, (-1 * step), step);
-            leds[i][j].b +=
-                LIMIT(leds_target[i][j].b - leds[i][j].r, (-1 * step), step);
+            leds[i][j].r += LIMIT(
+                (int16_t)((int16_t)leds_target[i][j].r - (int16_t)leds[i][j].r),
+                (-1 * step), step);
+            leds[i][j].g += LIMIT(
+                (int16_t)((int16_t)leds_target[i][j].g - (int16_t)leds[i][j].g),
+                (-1 * step), step);
+            leds[i][j].b += LIMIT(
+                (int16_t)((int16_t)leds_target[i][j].b - (int16_t)leds[i][j].b),
+                (-1 * step), step);
         }
     }
 }
@@ -246,6 +293,10 @@ void Strip_Mirage(int8_t mm_choice) {
         case 1:
             MM_Disco();
             break;
+
+        case 2:
+            MM_Fantasy();
+            break;
     }
 }
 
@@ -254,6 +305,10 @@ void Strip_Music(int8_t mm_choice, int16_t db_step) {
     switch (mm_choice) {
         case 0:
             MM_Disco(db_step);
+            break;
+
+        case 1:
+            MM_Fantasy(db_step);
             break;
     }
 }
@@ -275,7 +330,7 @@ void MM_Dusk() {
             if (leds[i][j] == leds_target[i][j]) {
                 leds_target[i][j] =
                     CHSV(random(H_Min, H_Max + 1), random(240, 255 + 1),
-                         random(230, 255 + 1));
+                         random(200, 255 + 1));
             }
         }
     }
@@ -283,15 +338,50 @@ void MM_Dusk() {
 
 // 蹦迪模式
 void MM_Disco(int16_t db_step) {
-    //根据输入分贝的大小决定步长
-    step = ((float)db_step / 1024.0f) * 10.0f;
+    step = 10;
 
     // 颜色变化较快
     for (int16_t i = 0; i < NUM_STRIPS; i++) {
         for (int16_t j = 0; j < NUM_LEDS_PER_STRIP; j++) {
             if (leds[i][j] == leds_target[i][j]) {
-                leds_target[i][j] = CRGB(random(255 + 1), 0, random(255 + 1));
+                leds_target[i][j] =
+                    CRGB(random(255 + 1), random(255 + 1), random(255 + 1));
             }
         }
     }
 }
+
+// 梦幻模式
+void MM_Fantasy(int16_t db) {
+    float color_rate =
+        MAX(10.0f, ((float)db - (float)VOICE_DOWN_DB)) / (float)VOICE_WIDTH_DB;
+
+    step = color_rate * 6.0f;
+
+    //根据输入分贝的大小决定颜色群数量
+    int16_t group_num = color_rate * (float)TOP_COLOR_GROUP_NUM;
+
+    group_num = MAX(1, group_num);
+
+    int16_t group_width = (float)NUM_LEDS_PER_STRIP / (float)group_num;
+
+    int16_t color_down_bias = 255.0f - (color_rate * 255.0f);
+
+    CRGB color_group[4] = {
+        {random(color_down_bias, 255 + 1), random(color_down_bias, 255 + 1), 0},
+        {random(color_down_bias, 255 + 1), random(color_down_bias, 255 + 1),
+         random(color_down_bias, 255 + 1)},
+        {random(color_down_bias, 255 + 1), 0, random(color_down_bias, 255 + 1)},
+        {0, random(color_down_bias, 255 + 1),
+         random(color_down_bias, 255 + 1)}};
+
+    // 颜色变化较快
+    for (int16_t i = 0; i < NUM_STRIPS; i++) {
+        for (int16_t j = 0; j < NUM_LEDS_PER_STRIP; j++) {
+            if (leds[i][j] == leds_target[i][j]) {
+                leds_target[i][j] = color_group[j / group_width % 4];
+            }
+        }
+    }
+}
+
